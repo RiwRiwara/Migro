f3afd8f1d9793c94d8d6be4d5ef9dda089f77425
RiwRiwara <awirut2629@gmail.com>
2024-04-24 23:12:07 +0700

    first commit

---
+++ views/home.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <script src="public/js/tailwind.js"></script>
+</head>
+<body>
+    <header >
+        <?php include_once  "components/navbar.php"; ?>
+    </header>
+</body>
+</html>
+
